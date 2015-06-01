c5156a85c7c5b2e14fa04474527a8a89ec1e89b8
sadmani <sabeen.admani@gmail.com>
2015-05-31 23:24:08 -0500

    Motor test script

---
+++ motor_pwm.X/motor.c
@@ -0,0 +1,130 @@
+#include<xc.h> // processor SFR definitions
+#include<sys/attribs.h>
+
+// DEVCFGs here
+
+// DEVCFG0
+#pragma config DEBUG = OFF // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // not boot write protect
+#pragma config CP = OFF // no code protect
+
+// DEVCFG1
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // free up secondary osc pins by turning sosc off
+#pragma config FPBDIV = DIV_1 // divide CPU freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1048576 // slowest wdt
+#pragma config WINDIS = OFF // no wdt window
+#pragma config FWDTEN = OFF // wdt off by default
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the CPU clock to 40MHz
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_20 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL
+#pragma config UPLLIDIV = DIV_2 // divide 8MHz input clock, then multiply by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON // USB clock on
+
+// DEVCFG3
+#pragma config USERID = 0x1234 // some 16 bit userid
+#pragma config PMDL1WAY = ON // not multiple reconfiguration, check this
+#pragma config IOL1WAY = ON // not multiple reconfiguration, check this
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // controlled by USB module
+
+int main(){
+    // startup
+	__builtin_disable_interrupts();
+	// set the CP0 CONFIG register to indicate that
+	// kseg0 is cacheable (0x3) or uncacheable (0x2)
+	// see Chapter 2 "CPU for Devices with M4K Core"
+	// of the PIC32 reference manual
+	__builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+	// no cache on this chip!
+
+	// 0 data RAM access wait states
+	BMXCONbits.BMXWSDRM = 0x0;
+
+	// enable multi vector interrupts
+	INTCONbits.MVEC = 0x1;
+
+	// disable JTAG to be able to use TDI, TDO, TCK, TMS as digital
+	DDPCONbits.JTAGEN = 0;
+
+	__builtin_enable_interrupts();
+    
+    // set up USER pin as input
+	ANSELBbits.ANSB13 = 0;
+	TRISBbits.TRISB13 = 1;
+
+    //Set phase pins
+    //   PH1--> B7
+    RPB7Rbits.RPB7R = 0b001; //Peripheral requires output pin
+    TRISBbits.TRISB7 = 0; //Make it an output
+    LATBbits.LATB7 = 1; //Set it high- this initializes as the forward direction
+
+    //   PH2-->B14
+    ANSELBbits.ANSB14 = 0; //Make it digital
+    RPB14Rbits.RPB14R = 0b001;
+    TRISBbits.TRISB14 = 0; //Make it an output
+    LATBbits.LATB14 = 1; //Set it high- this initializes as the forward direction    
+
+    //Set enable pins
+    //   EN1--> B5 
+    RPB5Rbits.RPB5R = 0b0101; //Set to OC2
+    TRISBbits.TRISB5 = 0; //Make it an output
+
+    //   EN2-->B15    
+    //ANSELBbits.ANSB15 = 0; //Make it digital
+    RPB15Rbits.RPB15R = 0b0101; //Set to OC1
+    TRISBbits.TRISB14 = 0; //Make it an output
+
+    //Set timers:
+    //This is to set up PWM frequency for OC pin- this will be altered using potentiometer
+	T2CONbits.TCKPS = 0;     // Timer2 prescaler N=1 (1:4)
+	PR2 = 1999;                 // period = (PR2+1) * N * 12.5 ns = 1 ms, 1 kHz
+	TMR2 = 0;     
+ 	OC1CONbits.OCM = 0b110;  // PWM mode without fault pin; other OC1CON bits are defaults
+    OC1CONbits.OCTSEL = 0; //Using Timer 2 for OC1
+    OC1RS = 500;               //duty cycle = OC1RS/(PR2+1) 25% duty cycle to begin with
+    OC1R = 500;                //initialize before turning OC1 on;        
+	T2CONbits.ON = 1;        // turn on Timer2
+	OC1CONbits.ON = 1;       // turn on OC1
+
+    //This is to set up PWM frequency for OC pin- this will be altered using potentiometer
+	T3CONbits.TCKPS = 0;     // Timer2 prescaler N=1 (1:4)
+	PR3 = 1999;                 // period = (PR2+1) * N * 12.5 ns = 1 ms, 1 kHz
+	TMR3 = 0;     
+ 	OC2CONbits.OCM = 0b110;  // PWM mode without fault pin; other OC1CON bits are defaults
+    OC2CONbits.OCTSEL = 1;     //Using Timer3 for OC3
+    OC2RS = 500;               //duty cycle = OC1RS/(PR2+1)
+    OC2R = 500;                //initialize before turning OC1 on;        
+	T3CONbits.ON = 1;        // turn on Timer2
+	OC2CONbits.ON = 1;       // turn on OC3
+    
+    __builtin_enable_interrupts();
+    
+    _CP0_SET_COUNT(0);
+    
+    while(_CP0_GET_COUNT()<40000000){
+        //motor 1 go backwards
+        LATBbits.LATB7 = 0;
+        OC1RS = 25*(PR2+1)/100;
+        OC2RS = 25*(PR3+1)/100;
+        _CP0_SET_COUNT(0);        
+    }
+    while(_CP0_GET_COUNT()<40000000){
+        //motor 1 go forwards
+        LATBbits.LATB7 = 1;
+        OC1RS = 25*(PR2+1)/100;
+        OC2RS = 25*(PR3+1)/100;
+        _CP0_SET_COUNT(0);        
+    }
+}
---
+++ motor_pwm.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=motor_pwm.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/motor_pwm.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=motorpwm.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/motorpwm.x.tar
